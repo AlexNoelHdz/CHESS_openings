25f7a4020274416b8e0c7f2b25d20e758ab8089d
Alejandro Noel Hernández <alejandro.noel.hernandez@gmail.com>
2023-11-12 12:57:53 -0600

    chess board working

--- testchess.py
+++ testchess.py
@@ -4,25 +4,60 @@ from chessboard import display
 
 # Descargar y extraer toda la carpeta en CHESS/stockfish
 # https://stockfishchess.org/download/windows/
-stockfish_path = "../CHESS/code/stockfish/stockfish-windows-x86-64.exe"
+stockfish_path = "../CHESS/stockfish/stockfish-windows-x86-64.exe"
 
 def human_move(move_input, board, displayed_board):
     try:
+        # Salir si el usuario envía x
         if move_input == "x":
-            return False
-        move = chess.Move.from_uci(move_input)
-        if move in board.legal_moves:
-            # push movement
-            board.push(move)
-            # Get fen from board
-            fen_human = board.fen()
-            display.check_for_quit()
-            # Show
-            display.update(fen_human, displayed_board)
-        else:
-            print("Movimiento no válido. Inténtalo de nuevo.")
+            display.terminate()
+
+        # push movement
+        board.push_san(move_input)
+        # Get fen from board
+        fen_human = board.fen()
+        display.check_for_quit()
+        # Show
+        display.update(fen_human, displayed_board)
+    except chess.InvalidMoveError:
+        print("Entrada no válida. Utiliza notación SAN (por ejemplo, 'e3').")
+    except chess.IllegalMoveError:
+        print("Movimiento Ilegal. Intente de nuevo")
+    except chess.AmbiguousMoveError as e:
+        print("Movimiento ambiguo, intente ser más específico. Por ejemplo especificar la letra origen (Rad1)")
     except ValueError:
-        print("Entrada no válida. Utiliza notación UCI (por ejemplo, 'e2e4').")
+        print("Error inesperado.")
+
+def stock_fish_move(board):
+    board_analysis = engine.analyse(board, limit=chess.engine.Limit(time=3))
+    best_move = board_analysis.get("pv")[0]
+    board.push(best_move)
+    fen_robot = board.fen()
+    display.check_for_quit()
+    display.update(fen_robot, displayed_board)
+
+def run_human_white_board(board, displayed_board):
+    # Si el turno es del jugador humano
+    if board.turn == chess.WHITE:
+        #Captura el movimiento del humano
+        move_input = input("Tu jugada: ")
+        human_move(move_input, board, displayed_board)
+    else:
+        # Turno del motor Stockfish
+        stock_fish_move(board)
+
+def run_human_black_board(board, displayed_board):
+    # Si el turno es del jugador humano
+    if board.turn == chess.BLACK:
+        # Flip board if necessary
+        if not displayed_board.flipped:
+            display.flip(displayed_board)
+        #Captura el movimiento del humano
+        move_input = input("Tu jugada: ")
+        human_move(move_input, board, displayed_board)
+    else:
+        # Turno del motor Stockfish
+        stock_fish_move(board)
 
 # Iniciar Stockfish engine
 with chess.engine.SimpleEngine.popen_uci(stockfish_path) as engine:
@@ -31,19 +66,12 @@ with chess.engine.SimpleEngine.popen_uci(stockfish_path) as engine:
     board = chess.Board()
     # Muestra el tablero en una ventana emergente con tamaño definido en el atributo size
     displayed_board = display.start()
+    # Pregunta al humano si quiere jugar con blancas o negras
+    b_or_w_input = input("Play white 'w' or black 'b': ")
+    play_game = run_human_black_board if b_or_w_input == 'b' else run_human_white_board
 
     # Mientras que el board no indique jaque mate
     while not board.is_game_over():
-        # Si el turno es del jugador humano
-        if board.turn == chess.WHITE:
-            #Captura el movimiento del humano
-            move_input = input("Tu jugada: ")
-            human_move(move_input, board, displayed_board)
-        else:
-            # Turno del motor Stockfish
-            result = engine.analyse(board, limit=chess.engine.Limit(time=3))
-            best_move = result.get("pv")[0]
-            board.push(best_move)
-            fen_robot = board.fen()
-            display.check_for_quit()
-            display.update(fen_robot, displayed_board)
+        play_game(board, displayed_board)
+
+
