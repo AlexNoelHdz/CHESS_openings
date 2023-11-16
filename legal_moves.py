3a5a9d9a5acff4232b40a53bf3d93b97fae50aac
Alejandro Noel Hernández <alejandro.noel.hernandez@gmail.com>
2023-11-16 15:43:51 -0600

    Obtener diagonales working

--- legal_moves.py
+++ legal_moves.py
@@ -1,13 +1,7 @@
 from chess import Board
+from chess import square_rank, square_file, square
 from chess import PIECE_NAMES, PIECE_TYPES, SQUARES, square_name
-
-class BoardPieces:
-    PAWN = 1
-    KNIGHT = 2
-    BISHOP = 3
-    ROOK = 4
-    QUEEN = 5
-    KING = 6
+from chess import PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING
 
 def get_legal_moves_from_piece_type(fen, piece_type):
     """
@@ -63,12 +57,66 @@ def pressure_points_san(fen):
     return pressure_points
 
 def get_legal_moves_san(fen):
+    """
+    Devuelve una lista con todos los movimientos legales de cada pieza en el tablero
+    para el jugador en turno
+
+    Args:
+    fen (str): La cadena FEN que representa la posición actual del tablero.
+
+    Returns:
+    list: Lista de las casillas de puntos de presión en notación SAN.
+    """
     legal_moves = {}
     # Fill legal moves according one piece type
     for piece_type in PIECE_TYPES:
         legal_moves[piece_type] = get_legal_moves_from_piece_type(fen, piece_type)
     return legal_moves
         
+def get_all_controled_diagonals(fen):
+    """
+    Cuenta el número de diagonales controladas por alfiles o reinas en un tablero de ajedrez,
+    dado por la notación FEN.
+    """
+    # Crear un tablero a partir de la notación FEN
+    tablero = Board(fen)
+    current_turn = tablero.turn # TRUE (WHITE) FALSE (BLACK)
+    diagonales_controladas = {}
+    diagonales_controladas_sum  = 0
+
+    # Por cada casilla entre las 64 posibles
+    for piece_type in BISHOP, QUEEN:
+        for square in tablero.pieces(piece_type, current_turn):
+            # Sumar diagonales controladas de alfiles y reinas
+            result = get_controlled_diagonals_by_square(square, tablero)
+            diagonales_controladas[square_name(square)] = result
+            diagonales_controladas_sum += result
+
+    return (diagonales_controladas, diagonales_controladas_sum)
+
+def get_controlled_diagonals_by_square(casilla, board: Board):
+    # Direcciones de las diagonales: superior izquierda, superior derecha, inferior izquierda, inferior derecha
+    direcciones = [(-1, -1), (-1, 1), (1, -1), (1, 1)]
+    controlled_diagonals = 0
+    # Por cada diagonal en las direcciones
+    for dx, dy in direcciones:
+        # Verificar las dos primeras casillas en cada dirección diagonal
+        for i in range(1, 3):
+            nueva_fila, nueva_columna = square_rank(casilla) + i * dx, square_file(casilla) + i * dy
+            # Si alguna de las dos casillas está fuera del tablero, la diagonal no esta  controlada o es inutil
+            if not (1 <= nueva_fila <= 8 and 1 <= nueva_columna <= 8):
+                break
+            nueva_casilla = square(nueva_columna, nueva_fila)
+            pieza_original = board.piece_at(casilla)
+            pieza_en_diagonal = board.piece_at(nueva_casilla)
+
+            # Si alguna de las dos casillas tiene una pieza del mismo color, la diagonal no está controlada o no es util
+            if pieza_en_diagonal and pieza_en_diagonal.color == pieza_original.color:
+                break
+        else:
+            controlled_diagonals += 1
+    return controlled_diagonals
+
 
 # Example FEN string
 # fen_example = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
@@ -81,3 +129,7 @@ for piece_type in PIECE_TYPES:
 
 pressure_points = pressure_points_san(fen_example)
 print(f"Puntos de presión ({len(pressure_points)}): {pressure_points}")
+
+diagonals_fen_test = "rnbqkb1r/pp4pp/4pp1n/2pp4/3P1B2/3Q2P1/PPP1PP1P/RN2KBNR w KQkq - 0 7"
+diagonals, diagonals_sum = get_all_controled_diagonals(diagonals_fen_test)
+print(f"Diagonales controladas ({diagonals_sum}): {diagonals}")
